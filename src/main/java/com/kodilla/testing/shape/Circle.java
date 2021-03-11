d4d035550f7f682920ac6b1e809bcb9f5dd2b583
Oskar Ferens <oskar.ferens1@gmail.com>
2021-03-11 21:54:25 +0100

    6.4 Shape

---
+++ src/main/java/com/kodilla/testing/shape/Circle.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.shape;
+
+public class Circle implements Shape {
+
+    double r;
+    public Circle (double r) {
+        this.r = r;
+    }
+
+    @Override
+    public String getFigureName() {
+        return "circle";
+    }
+
+    @Override
+    public Double getArea() {
+        return Math.PI * r * r;
+    }
+}
---
+++ src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -0,0 +1,73 @@
+package com.kodilla.testing.shape;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
+
+
+class ShapeCollectorTestSuite {
+    @Nested
+    @DisplayName("Collector's addFigure method collects input shape in owns memory.")
+    @Test
+    void addFigureTest () {
+        //Given
+        Shape square = new Square(10);
+        ShapeCollector collector = new ShapeCollector();
+        //When
+        collector.addFigure(square);
+        Shape firstShape = collector.getFigure(0);
+        //Then
+        Assertions.assertNotNull(firstShape);
+        Assertions.assertEquals(firstShape.getFigureName(),"square");
+    }
+
+    @DisplayName("collector removes Figure method; delete input shape from it owns memory.")
+    @Test
+    void removeFigureTest () {
+        Shape square = new Square(10);
+        Shape circle = new Circle(4);
+        Shape triangle = new Triangle(5,10);
+        ShapeCollector collector = new ShapeCollector();
+
+        collector.addFigure(square);
+        collector.addFigure(circle);
+        collector.addFigure(triangle);
+
+        collector.removeFigure(square);
+
+        Assertions.assertEquals(2,collector.list.size());
+    }
+
+    @DisplayName("Using getFigure to take one from the ArrayList.")
+    @Test
+    void getFigure () {
+        Shape square = new Square(10);
+        ShapeCollector collector = new ShapeCollector();
+        collector.addFigure(square);
+
+        collector.getFigure(0);
+
+        Assertions.assertEquals(square,collector.getFigure(0));
+    }
+
+    @DisplayName("Printing all objects in ArrayList (shapes) as one String.")
+    @Test
+    void showFigures () {
+        //Given
+        ShapeCollector collector = new ShapeCollector();
+        Triangle triangle = new Triangle(5,10);
+        Square square = new Square(10);
+        Circle circle = new Circle(4);
+
+
+        collector.addFigure(triangle);
+        collector.addFigure(square);
+        collector.addFigure(circle);
+        //When
+        collector.showFigures();
+        String expected = "triangle,square,circle";
+        //Then
+        Assertions.assertEquals(expected,collector.showFigures());
+    }
+}
