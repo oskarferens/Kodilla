6ad6b8176bafa9dab1313c73939407e4465988e5
Oskar Ferens <oskar.ferens1@gmail.com>
2021-04-18 23:05:32 +0200

    8.3 SecondChallenge Throw,Throws

---
+++ kodilla-excepion/src/main/java/com/kodilla/exception/test/Flight.java
@@ -0,0 +1,11 @@
+package com.kodilla.exception.test;
+
+public class Flight {
+    String departureAirport;
+    String arrivalAirport;
+
+    public Flight(String departureAirport,String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+        this.departureAirport = departureAirport;
+    }
+}
