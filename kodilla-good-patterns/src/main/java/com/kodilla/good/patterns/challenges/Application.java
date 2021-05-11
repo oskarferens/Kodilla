72b9c956e4689af732679f35591011cf46ca2b5c
Oskar Ferens <oskar.ferens1@gmail.com>
2021-05-11 15:56:03 +0200

    13.1 good-patterns

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Application.java
@@ -2,6 +2,10 @@ package com.kodilla.good.patterns.challenges;
 
 public class Application {
     public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
 
+        movieStore.getMovies().values().stream()
+                .flatMap(strings -> strings.stream())
+                .forEach(s -> System.out.print(s+"! "));
     }
 }
