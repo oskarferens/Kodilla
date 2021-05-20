580249ef1a74171beec722e0d28aa4d82e341ff2
Oskar Ferens <oskar.ferens1@gmail.com>
2021-05-20 20:07:15 +0200

    13.5. Flight

--- kodilla-good-patterns/src/main/java/Flight/Application.java
+++ kodilla-good-patterns/src/main/java/Flight/Application.java
@@ -3,6 +3,10 @@ package Flight;
 public class Application {
 
     public static void main(String[] args) {
-        
+        FlightSearchEngine flightSearchEngine = new FlightSearchEngine();
+
+        flightSearchEngine.searchDepartureFromAirport("Gdańsk");
+        flightSearchEngine.searchToArrivalAirport("Wrocław");
+        flightSearchEngine.searchWithStopoverFlight("Kraków", "Wrocław", "Gdańsk");
     }
 }
