a4e434e7b0a501be58782e7c30b8109c56dd96ff
Oskar Ferens <oskar.ferens1@gmail.com>
2021-05-23 20:35:34 +0200

    14.2 Calculator

--- kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
@@ -4,6 +4,7 @@ import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
 import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class KodillaSpringApplicationTests {
+
 
     @Test
     void testCircleLoadedIntoContainer() {
