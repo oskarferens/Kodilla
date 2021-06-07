33756eee25dcb86f13ee1c9c47b85ee8abfe62b6
Oskar Ferens <oskar.ferens1@gmail.com>
2021-06-07 19:20:23 +0200

    16.6 jdbc

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -8,27 +8,28 @@ import java.util.Properties;
 
 public class DbManager {
 
-    private Connection conn;
-    private static DbManager dbManagerInstance;
+    private Connection conn;                                          // [1]
+    private static DbManager dbManagerInstance;                       // [2]
 
-    private DbManager() throws SQLException {
-        Properties connectionProps = new Properties();
-        connectionProps.put("user", "kodilla_user");
-        connectionProps.put("password", "kodilla_Pass123");
-        conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/kodilla_course?serverTimezone=Europe/Warsaw" +
-                        "&useSSL=False",
-                connectionProps);
-    }
+    private DbManager() throws SQLException {                         // [3]
+        Properties connectionProps = new Properties();                 // [4]
+        connectionProps.put("user", "kodilla_user");                   // [5]
+        connectionProps.put("password", "kodilla_password");           // [6]
+        conn = DriverManager.getConnection(                            // [7]
+                "jdbc:mysql://localhost:3306/kodilla_course?serverTimezone=Europe/Warsaw" +  // [8]
+                        "&useSSL=False",                                             // [9]
+                connectionProps);                                              // [10]
+    }                                                                 // [11]
 
-    public static DbManager getInstance() throws SQLException {
-        if (dbManagerInstance == null) {
-            dbManagerInstance = new DbManager();
-        }
-        return dbManagerInstance;
-    }
+    public static DbManager getInstance() throws SQLException {       // [12]
+        if (dbManagerInstance == null) {                               // [13]
+            dbManagerInstance = new DbManager();                        // [14]
+        }                                                              // [15]
+        return dbManagerInstance;                                      // [16]
+    }                                                                 // [17]
 
-    public Connection getConnection() {
-        return conn;
-    }
+    public Connection getConnection() {                               // [18]
+        return conn;                                                   // [19]
+    }                                                                 // [20]
 }
+
